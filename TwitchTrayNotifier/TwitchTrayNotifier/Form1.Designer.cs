6fef4ae786834433410b888a2640fe85f89477b3
Ben Suskins <bensuskins@gmail.com>
2015-03-13 18:43:37 +0000

    Initial Commit

---
+++ TwitchTrayNotifier/TwitchTrayNotifier/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TwitchTrayNotifier
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(182, 29);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter your Twitch details here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(76, 31);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 4;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 113);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SettingsForm";
+            this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+    }
+}
+
---
+++ TwitchTrayNotifier/TwitchTrayNotifier/Form1.cs
@@ -0,0 +1,108 @@
+﻿#region Imports
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+#endregion
+///Twitch Live stream notifier program
+///By Ben Suskins 12/03/15
+///Version 0.1
+///Known Issues:
+///-Doesn't Work
+namespace TwitchTrayNotifier
+{
+    public partial class SettingsForm : Form
+    {
+        #region Global Variables
+        NotifyIcon twitchActiveTray;
+        Icon twitchIcon;
+        Thread twitchLiveWorkerThread;
+        string Username;
+        #endregion
+
+        #region Form Hiding & Setting up tray notification
+        public SettingsForm()
+        {
+            InitializeComponent();
+
+            //Set up the icon image
+            twitchIcon = new Icon("twitch.ico");
+
+            //Make notification tray icon active
+            twitchActiveTray = new NotifyIcon();
+            twitchActiveTray.Icon = twitchIcon;
+            twitchActiveTray.Visible = true;
+
+            //Create context menu items to the notification tray icon
+            MenuItem progNameMenuItem = new MenuItem("Twitch Live Notifier - By Ben Suskins 2015 V0.1");
+            MenuItem breakMenuItem = new MenuItem("-");
+            MenuItem settingsMenuItem = new MenuItem("Settings");
+            MenuItem quitMenuItem = new MenuItem("Quit");
+
+            //Add context menu items to the tray icon
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add(progNameMenuItem);
+            contextMenu.MenuItems.Add(breakMenuItem);
+            contextMenu.MenuItems.Add(settingsMenuItem);
+            contextMenu.MenuItems.Add(quitMenuItem);
+            twitchActiveTray.ContextMenu = contextMenu;
+
+            //Allow the buttons to have a function when clicked
+            settingsMenuItem.Click += settingsMenuItem_Click;
+            quitMenuItem.Click += quitMenuItem_Click;
+
+            //Minimize the form and then stop it from showing in task bar
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+            //Start up thread to check if channel live
+            twitchLiveWorkerThread = new Thread(new ThreadStart(TwitchChannelLiveThread));
+            twitchLiveWorkerThread.Start();
+
+        }
+
+
+        #endregion
+
+        #region Context Menu, Quit / Settings Functions
+        //When settings menu item is clicked allow them to enter twitch details
+        void settingsMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+
+        }
+        //When quit menu item is clicked exit the program and dispose of tray icons
+        void quitMenuItem_Click(object sender, EventArgs e)
+        {
+            twitchIcon.Dispose();
+            this.Close();
+        }
+        #endregion
+
+        #region Thread to check if live
+        private void TwitchChannelLiveThread()
+        {
+
+        }
+        #endregion
+
+        #region Username storage / Application
+        //Take the username from textbox and store in file for use when program closed and save it
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //On click take txtUsername and save to file
+            Username = txtUsername.Text;
+            MessageBox.Show("Your twitch username is: " + Username);
+            this.WindowState = FormWindowState.Minimized;
+                
+        }
+        #endregion
+
+    }
+}
